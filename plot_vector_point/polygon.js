b2bcc98700e3acd6ace967149acc0f7f18db7440
kwon <skgus2624@gmail.com>
2019-01-04 14:11:52 +0900

    segmentation data to polygon

--- plot_vector_point/polygon.js
+++ plot_vector_point/polygon.js
@@ -1,18 +1,11 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 586,
-    height = 640;
+    width = 1000,
+    height = 1000;
 
 	var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g");
-
-
-  var x = d3.scaleLinear().range([0, 586]);
-	var y = d3.scaleLinear().range([640, 0]);
-
-  x.domain([0, 586]);
-  y.domain([0, 640]);
 
   var point = {"x": 24, "y": 31}
 
@@ -26,11 +19,52 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
         {"x":500,"y":500},
         {"x":10,"y":500}];
 
+  var list_poly = {"points":[[37.31, 373.02],
+  [57.4, 216.61],
+ [67.44, 159.21],
+ [77.49, 113.29],
+ [91.84, 86.03],
+ [123.41, 84.59],
+ [162.15, 96.07],
+ [215.25, 86.03],
+ [261.17, 70.24],
+ [285.5, 68.81],
+ [337.22, 68.81],
+ [411.84, 93.2 ],
+ [454.89, 107.55],
+ [496.5 , 255.35],
+ [513.72, 262.53],
+ [552.47, 292.66],
+ [586., 324.23],
+ [586., 381.63],
+ [586., 449.08],
+ [586., 453.38],
+ [578.3 , 616.97],
+ [518.03, 621.27],
+ [444.84, 624.14],
+ [340.09, 625.58],
+ [136.32, 625.58],
+ [1.43, 632.75],
+ [7.17, 555.26],
+ [5.74, 414.64]],
+ "object": "bear",
+ "width": 586,
+ "height":640}
 
-    svg.append('image')
-    .attr('xlink:href', '000000000285.jpg')
-    .attr('width', width)
-    .attr('height', height)
+  var img_file = '000000000285.jpg';
+
+
+  var image = svg.append('image')
+    .attr('xlink:href', img_file)
+    .attr('width', this.naturalWidth)
+    .attr('height', this.naturalHeight)
+
+  var x = d3.scaleLinear().range([0, list_poly.width]);
+    var y = d3.scaleLinear().range([list_poly.height, 0]);
+
+  x.domain([0, list_poly.width]);
+  y.domain([0, list_poly.height]);
+
 
   svg.selectAll("polygon")
     .data([poly])
@@ -39,8 +73,13 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
         return d.map(function(d) {
             return [x(d.x),y(d.y)].join(",");
         }).join(" ");
+
     });
 
-  polygon.style("fill-opacity", .2);
-
-
+  svg.selectAll("polygon")
+  .style("fill-opacity", .5)
+  //.style("stroke", "black")
+  //.style("stroke-width", 5)
+  .style("fill",function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+    });
