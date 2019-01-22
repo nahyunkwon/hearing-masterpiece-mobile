9fa2d18d0bf7662f25bdc8d057b00d7642ddc10a
kwon <skgus2624@gmail.com>
2019-01-23 02:31:30 +0900

    segmentation keyError

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,26 +1,10 @@
 import json
-import time
-import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Polygon
 import numpy as np
-import copy
-import itertools
-
-import os
-from collections import defaultdict
-import sys
-from pprint import pprint
-
-from pycocotools.coco import COCO
-import numpy as np
-import skimage.io as io
-import matplotlib.pyplot as plt
-import pprint
 
 dataset = json.load(open("./plot_image_segmentation_web/instances_val2017.json", 'r'))
 
-image_ids = [139]
+image_ids = [139, 285, 18380]
+
 '''
 def get_metadata(dataset, image_ids):
     image = {}
@@ -45,42 +29,68 @@ print(image, annotations)
 
 '''
 
-categories = dataset['categories']
 
-annotations = []
+def get_metadata(dataset, image_ids):
+    categories = dataset['categories']
 
-for img in dataset['images']:
-    if img['id'] == 139:
-        image = img
+    images = []
 
-for ann in dataset['annotations']:
-    if ann['image_id'] == 139:
-        #annotations.append(ann.copy())
-        seg = ann['segmentation'][0]
-        ann['segmentation'] = np.array(seg).reshape((int(len(seg) / 2), 2)).tolist()
+    for i in image_ids:
+        annotations = []
 
-        [bbox_x, bbox_y, bbox_w, bbox_h] = ann['bbox']
-        bbox_point = [[bbox_x, bbox_y], [bbox_x, bbox_y + bbox_h], [bbox_x + bbox_w, bbox_y + bbox_h],
-                [bbox_x + bbox_w, bbox_y]]
-        bbox = np.array(bbox_point).reshape((4, 2)).tolist()
+        for img in dataset['images']:
+            if img['id'] == i:
+                image = img
 
-        ann['bbox'] = bbox
-        
-        for cat in categories:
-            if ann['category_id'] == cat['id']:
-                ann['category'] = cat['name']
+        for ann in dataset['annotations']:
+            if ann['image_id'] == i:
+                ann_copy = ann
+                print(ann_copy)
+                if(len(ann_copy['segmentation']) > 1):
+                    seg = ann_copy['segmentation'][0]
+                else:
+                    seg = ann_copy['segmentation']
 
-        for key in ['iscrowd', 'image_id', 'category_id', 'id']:
-            del ann[key]
+                seg = ann_copy['segmentation'][0]
 
-        annotations.append(ann.copy())
+                ann_copy['segmentation'] = np.array(seg).reshape((int(len(seg) / 2), 2)).tolist()
 
-sorted_ann = sorted(annotations, key=lambda k: k['area'], reverse=True)
+                [bbox_x, bbox_y, bbox_w, bbox_h] = ann_copy['bbox']
+                bbox_point = [[bbox_x, bbox_y], [bbox_x, bbox_y + bbox_h], [bbox_x + bbox_w, bbox_y + bbox_h],
+                              [bbox_x + bbox_w, bbox_y]]
+                bbox = np.array(bbox_point).reshape((4, 2)).tolist()
 
-for key in ['license', 'date_captured', 'flickr_url', 'coco_url']:
-    del image[key]
+                ann_copy['bbox'] = bbox
 
-image['annotations'] = sorted_ann
+                for cat in categories:
+                    if ann['category_id'] == cat['id']:
+                        ann['category'] = cat['name']
 
-with open('result.json', 'w') as fp:
-    json.dump(image, fp, sort_keys=True, indent=1, separators=(',', ': '))
+                #for key in ['iscrowd', 'image_id', 'category_id', 'id']:
+                 #   del ann[key]
+
+                annotations.append(ann_copy.copy())
+
+        sorted_ann = sorted(annotations, key=lambda k: k['area'], reverse=True)
+
+        for key in ['license', 'date_captured', 'flickr_url', 'coco_url']:
+            del image[key]
+
+        image['annotations'] = sorted_ann
+
+        print(image)
+
+        images.append(image)
+
+    print(images)
+
+    total_data = {}
+
+    total_data['images'] = images
+
+    print(total_data)
+
+    with open('result.json', 'w') as fp:
+        json.dump(total_data, fp, sort_keys=False, indent=1, separators=(',', ': '))
+
+get_metadata(dataset, image_ids)
