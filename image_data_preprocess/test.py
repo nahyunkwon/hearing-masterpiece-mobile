0a49d1b9ec8d49519d6cc1069730ecf8765dba28
KwonNH <skgus2624@gmail.com>
2019-12-26 16:50:35 +0900

    add howtouse, about page

--- image_data_preprocess/test.py
+++ image_data_preprocess/test.py
@@ -10,14 +10,3 @@ import pandas as pd
 import pprint
 import json
 
-raw_data = {'name': ['Willard Morris', 'Al Jennings', 'Omar Mullins', 'Spencer McDaniel'],
-'age': [20, 19, 22, 21],
-'favorite_color': ['blue', 'red', 'yellow', "green"],
-'grade': [88, 92, 95, 70]}
-df = pd.DataFrame(raw_data)
-
-
-#print(df.iloc[0]['name'])
-
-df.at[0, 'name'] = "new"
-print(df)
